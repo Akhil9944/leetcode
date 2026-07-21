df5d970abbca4a379fed41358ac1ec8c77a803a7
Akhil <138991705+Akhil9944@users.noreply.github.com>
2026-07-21 18:33:01 +0530

    Time: 23 ms (35.39%) | Memory: 13.6 MB (37.17%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack=[-1]
+        ans=0
+        for i,ch in enumerate(s):
+            if ch=='(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    ans=max(ans,i-stack[-1])
+        return ans
+        
