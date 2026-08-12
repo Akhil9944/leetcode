23a2254b13b1d47f7e7944406fa3aad421145dec
Akhil <138991705+Akhil9944@users.noreply.github.com>
2026-08-12 14:00:40 +0530

    Time: 141 ms (60.34%) | Memory: 15.5 MB (29.48%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -5,19 +5,18 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        count = {}
-        left = 0
-        max_freq = 0
-        ans = 0
+        left=0
+        count={}
+        max_freq=0
+        maxi=0
 
         for right in range(len(s)):
-            count[s[right]] = count.get(s[right], 0) + 1
-            max_freq = max(max_freq, count[s[right]])
+            count[s[right]]=count.get(s[right],0)+1
+            max_freq=max(max_freq,count[s[right]])
 
-            while (right - left + 1) - max_freq > k:
-                count[s[left]] -= 1
-                left += 1
+            while(right-left+1)-max_freq>k:
+                count[s[left]]-=1
+                left+=1
+            maxi=max(maxi,right-left+1)
 
-            ans = max(ans, right - left + 1)
-
-        return ans
+        return maxi
